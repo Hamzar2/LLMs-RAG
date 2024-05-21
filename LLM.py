afd7b4a8990e03af598f44f4bc96a0ba83431872
Hamzar2 <hamzaidrissidr2@gmail.com>
2024-05-21 23:11:14 +0100

    update

--- LLM.py
+++ LLM.py
@@ -2,40 +2,42 @@ from torch import cuda, bfloat16
 import transformers
 from accelerate import disk_offload
 
-
 def LLM():
-    model_id = 'GreenBitAI/LLaMA-7B-2bit'
+    model_id = 'GreenBitAI/LLaMA-7B-2bit' 
 
-    device = 'cpu'
+    # Device Selection (GPU if available, otherwise CPU)
+    device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
 
-    # set quantization configuration to load large model with less GPU memory
-    # this requires the `bitsandbytes` library
+    # 1. BitsAndBytes Configuration (Important for 2-bit quantization) only for GPU
     bnb_config = transformers.BitsAndBytesConfig(
-        load_in_4bit=False,
+        load_in_4bit=False, 
         bnb_4bit_quant_type='nf4',
         bnb_4bit_use_double_quant=True,
         bnb_4bit_compute_dtype=bfloat16
     )
 
-    # begin initializing HF items, need auth token for these
-    hf_auth = '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'
+    # 2. Initialize Model Config (Optional, but recommended)
+    hf_auth = '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'  
     model_config = transformers.AutoConfig.from_pretrained(
         model_id,
         token=hf_auth,
-        pad_token_id = 0
+        pad_token_id=0 
     )
-
+    print("Starting model loading...")
+    
+    # 3. Load the Model (using BitsAndBytes and disk_offload)
     model = transformers.AutoModelForCausalLM.from_pretrained(
         model_id,
         trust_remote_code=True,
-        config=model_config, #quantization_config=bnb_config,
+        config=model_config,#quantization_config=bnb_config,  # Apply the BitsAndBytes config
         device_map='auto',
         token=hf_auth,
-        offload_folder=r'rag-tutorial-v2\offload',
-        force_download=False
+        offload_folder='offload',  
+        force_download=False 
     )
-    # model = disk_offload(model)
-    # model.eval()
-    print(f"Model loaded on {device}")
+    print("Model loaded successfully.")
 
+    model = disk_offload(model)
+    model.eval()
+    print(f"Model loaded on {device}")
     return model
--- query_data.py
+++ query_data.py
@@ -39,7 +39,7 @@ def query_rag(query_text: str):
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    # print(prompt)
+    print(prompt)
 
     model = LLM()
 
